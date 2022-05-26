8ca54ac65cd430051cc43aaf1382b9400489015c
Amanda0722 <106169960+Amanda0722@users.noreply.github.com>
2022-05-26 16:16:00 -0400

    Update alta3research-python3daycert01.py

--- alta3research-python3daycert01.py
+++ alta3research-python3daycert01.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
+#A.R. 20220526v1
+#import date and time
+:
+from datetime import datetime
 
+now = datetime.now()
+
+#UTC time
+current_time = now.strftime("%H:%M:%S")
+
+#print UTC time to screen
+print("Current UTC Time =", current_time)
